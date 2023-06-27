59350c50110d823b71c76ce35c65061fee0872d6
Filip Stojakovic <filip.stojakovic1@gmail.com>
2023-06-27 10:19:28 +0200

    quick fix

--- src/main/java/org/etfbl/support/webshopsupport/bean/LoginBean.java
+++ src/main/java/org/etfbl/support/webshopsupport/bean/LoginBean.java
@@ -1,12 +1,7 @@
 package org.etfbl.support.webshopsupport.bean;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.io.Serializable;
 
-@Getter
-@Setter
 public class LoginBean implements Serializable {
 
     private static final long serialVersionUID = -4021922494651503066L;
@@ -14,4 +9,27 @@ public class LoginBean implements Serializable {
     private String username;
     private String password;
 
+    public LoginBean() {
+    }
+
+    public LoginBean(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
